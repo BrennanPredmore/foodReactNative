f8de30e5f33f03990f4d38370cf7b05ba6ddbf87
Brennan Predmore <brennnann@gmail.com>
2020-08-18 20:03:06 -0500

    yarn issue installing dependencies

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
 const navigation = createStackNavigator({
-  
-})
+  Search: SearchScreen
+}, {
+  initialRouteName: Search,
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
+});
+
+export default createAppContainer(navigator);
