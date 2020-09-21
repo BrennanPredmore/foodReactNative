93e4cfd0c1a92614730d367cd77c7b6b0768ee57
Brennan Predmore <brennnann@gmail.com>
2020-09-21 11:07:09 -0500

    try/catch err init

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,7 @@ const SearchScreen = () => {
   const [result, setResults] = useState('');
 
   const searchApi = async () => {
+    try {
     const response = await yelp.get('/search', {
         params: {
             limit: 50,
@@ -16,6 +17,9 @@ const SearchScreen = () => {
         }
     });
     setResults(response.data.businesses);
+  } catch (err) {
+    console.log(err)
+  };
   };
 
   return (
