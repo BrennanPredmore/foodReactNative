f1a26ea118196dea5b2e4396069396fafaf38341
Brennan Predmore <brennnann@gmail.com>
2020-09-21 10:21:52 -0500

    bug fixed for params

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,13 +8,13 @@ const SearchScreen = () => {
   const [result, setResults] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search'), {
+    const response = await yelp.get('/search', {
         params: {
             limit: 50,
             term,
             location: 'san jose'
         }
-    }
+    });
     setResults(response.data.businesses);
   };
 
